105a47980154f9cd4df51463672b58ccab8b6ed6
Rafy <Rafy@DESKTOP-ADC4ST3>
2019-03-11 17:03:34 +0200

    still working on this problem

--- Counting Valleys/Program.cs
+++ Counting Valleys/Program.cs
@@ -8,8 +8,44 @@ namespace Counting_Valleys
 {
     class Program
     {
+        // Complete the countingValleys function below.
+        static int countingValleys(int n, string s)
+        {
+
+            int level = 0;
+            int valleyCount = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'U')
+                {
+                    level += 1;
+                }
+                else if (s[i] == 'D')
+                {
+                    level -= 1;
+                }
+                if (level == 0)
+                {
+                    valleyCount += 1;
+                }
+            }
+            return valleyCount;
+        }
+
         static void Main(string[] args)
         {
+            // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string s = Console.ReadLine();
+
+            int result = countingValleys(n, s);
+
+            Console.WriteLine(result);
+
+            //   textWriter.Flush();
+            //textWriter.Close();
         }
     }
 }
